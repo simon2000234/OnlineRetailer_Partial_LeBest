895b018d0fe670df888f2e58dcbec0d6c31dc478
Simon <warcraft.simon@gmail.com>
2021-02-26 12:31:20 +0100

    CustomerRepoWorks

---
+++ CustomerAPI/Controllers/CustomersController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerAPI.Data;
+using CustomerAPI.Models;
+
+namespace CustomerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : Controller
+    {
+        private readonly IRepository<Customer> repo;
+
+        public CustomersController(IRepository<Customer> repos)
+        {
+            repo = repos;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return repo.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public IActionResult Get(int id)
+        {
+            var item = repo.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+
+            var newCustomer = repo.Add(customer);
+
+            return CreatedAtRoute("GetCustomer", new { id = newCustomer.Id }, newCustomer);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Customer customer)
+        {
+            if (customer == null || customer.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var modifiedProduct = repo.Get(id);
+
+            if (modifiedProduct == null)
+            {
+                return NotFound();
+            }
+
+            modifiedProduct.Name = customer.Name;
+            modifiedProduct.Phone = customer.Phone;
+            modifiedProduct.BillingAddress = customer.BillingAddress;
+            modifiedProduct.CreditStanding = customer.CreditStanding;
+            modifiedProduct.Email = customer.Email;
+            modifiedProduct.ShippingAddress = customer.ShippingAddress;
+
+            repo.Edit(modifiedProduct);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (repo.Get(id) == null)
+            {
+                return NotFound();
+            }
+
+            repo.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ CustomerAPI/Startup.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerAPI.Data;
+using CustomerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // In-memory database:
+            services.AddDbContext<CustomerApiContext>(opt => opt.UseInMemoryDatabase("CustomersDb"));
+
+            // Register repositories for dependency injection
+            services.AddScoped<IRepository<Customer>, CustomerRepository>();
+
+            // Register database initializer for dependency injection
+            services.AddTransient<IDbInitializer, DbInitializer>();
+
+
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Initialize the database
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                // Initialize the database
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetService<CustomerApiContext>();
+                var dbInitializer = services.GetService<IDbInitializer>();
+                dbInitializer.Initialize(dbContext);
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

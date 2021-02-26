6bc9452fa977644aeeeedee41d31754ede73247e
CasperLC <43999061+CasperLC@users.noreply.github.com>
2021-02-26 11:59:22 +0100

    ja.

---
+++ OrderApi/Models/OrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Models
+{
+    public class OrderLine
+    {
+        public int NoOfItems { get; set; }
+        public int OrderId { get; set; }
+
+        public Order Order { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}

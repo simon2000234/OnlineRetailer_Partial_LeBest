8f605b23faab8ffbd05bd8b3ddd2a9db73fcc311
CasperLC <43999061+CasperLC@users.noreply.github.com>
2021-03-12 11:19:42 +0100

    Converters for bounded context

---
+++ OrderApi/Models/OrderConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SharedModels;
+
+namespace OrderApi.Models
+{
+    public class OrderConverter
+    {
+        public Order Convert(OrderDTO sharedOrder)
+        {
+            var convertedOrderLine = new List<OrderLine>();
+
+            foreach (var orderLine in sharedOrder.OrderLines)
+            {
+                convertedOrderLine.Add(Convert(orderLine));
+            }
+
+            return new Order
+            {
+                Id = sharedOrder.Id,
+                customerId = sharedOrder.customerId,
+                Date = sharedOrder.Date,
+                Status = (Order.OrderStatus)sharedOrder.Status,
+                OrderLines = convertedOrderLine
+            };
+        }
+
+        public OrderDTO Convert(Order hiddenOrder)
+        {
+            var convertedOrderLineDTO = new List<OrderLineDTO>();
+
+            foreach (var orderLine in hiddenOrder.OrderLines)
+            {
+                convertedOrderLineDTO.Add(Convert(orderLine));
+            }
+
+            return new OrderDTO
+            {
+                Id = hiddenOrder.Id,
+                customerId = hiddenOrder.customerId,
+                Date = hiddenOrder.Date,
+                Status = (OrderDTO.OrderStatus)hiddenOrder.Status,
+                OrderLines = convertedOrderLineDTO
+            };
+        }
+
+        public OrderLine Convert(OrderLineDTO sharedOrderLine)
+        {
+            return new OrderLine
+            {
+                id = sharedOrderLine.id,
+                OrderId = sharedOrderLine.OrderId,
+                ProductId = sharedOrderLine.ProductId,
+                Quantity = sharedOrderLine.Quantity
+            };
+        }
+
+        public OrderLineDTO Convert(OrderLine hiddenOrderLine)
+        {
+            return new OrderLineDTO
+            {
+                id = hiddenOrderLine.id,
+                OrderId = hiddenOrderLine.OrderId,
+                ProductId = hiddenOrderLine.ProductId,
+                Quantity = hiddenOrderLine.Quantity
+            };
+        }
+
+
+    }
+}
---
+++ SharedModels/OrderDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedModels
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public int? customerId { get; set; }
+        public OrderStatus Status { get; set; }
+        public IList<OrderLineDTO> OrderLines { get; set; }
+
+        public enum OrderStatus
+        {
+            cancelled,
+            completed,
+            shipped,
+            paid
+        }
+    }
+
+    public class OrderLineDTO
+    {
+        public int id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
